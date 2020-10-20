39ce0e12c477ab65fbdbd221117e6c5fdcadd38a
wheatiez <logan.wheat77@gmail.com>
2020-10-19 21:23:30 -0500

    update

--- Args.java
+++ Args.java
@@ -1,5 +1,17 @@
 class Args implements Token
 {
+    Expr expr;
+    Args args;
+
+    public Args(Expr e, Args a)
+    {
+        expr = e;
+        args = a;
+    }
+    public Args(Expr e)
+    {
+        expr = e;
+    }
 
     public String toString(int t)
     {
