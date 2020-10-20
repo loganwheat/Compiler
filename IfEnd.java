59d122ec91533b0bbaa01f1c41f6b86dbbd95d04
wheatiez <logan.wheat77@gmail.com>
2020-10-19 21:14:20 -0500

    updates to java files

--- IfEnd.java
+++ IfEnd.java
@@ -1,8 +1,25 @@
 class IfEnd implements Token
 {
-    
+    Stmt stmt;
+    boolean hasStmt;
+
+    public IfEnd(Stmt s)
+    {
+        hasStmt = true;
+        stmt = s;
+    }
+    public IfEnd()
+    {
+        hasStmt = false;
+    }
+
     public String toString(int t)
     {
-
+        if(!hasStmt) {
+            return "fi";
+        }
+        else {
+            return "else " + stmt.toString(t) + " fi";
+        }
     }
 }
--- Name.java
+++ Name.java
@@ -1,8 +1,26 @@
 class Name implements Token
 {
+    String id;
+    Expr expr;
+
+    public Name(String i, Expr e)
+    {
+        id = i;
+        expr = e;
+    }
+    public Name(String i)
+    {
+        id = i;
+        expr = null;
+    }
 
     public String toString(int t)
     {
-        
+        if(expr != null){
+            return id + "[" + expr.toString(t) + "]";
+        }
+        else {
+            return id;
+        }
     }
 }
